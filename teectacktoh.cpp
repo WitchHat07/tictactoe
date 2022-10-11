ea6457fea4a6985740a1f822f0de5789c41c99f3
ButteryPoptart <115423877+ButteryPoptart@users.noreply.github.com>
2022-10-11 10:29:10 -0500

    Add files via upload
    1.1 with vectors!

--- teectacktoh.cpp
+++ teectacktoh.cpp
@@ -4,18 +4,16 @@
 using namespace std;
 
 void intro_text();
-void display_gameboard(char oneP, char twoP, char threeP, char fourP, char fiveP, char sixP, char sevenP, char eightP, char nineP);
+void display_gameboard(vector<char> boardish);
 
 int main() {
 	intro_text();
-	/* TODO: integrate gameboard into an array or vector of some sort
 	vector<char> board;
 	for (int i = 1; i <= 9; i++) {
 		board.push_back('-');
 	}
-	*/
+	display_gameboard(board);
 	char one = '-', two = '-', three = '-', four = '-', five = '-', six = '-', seven = '-', eight = '-', nine = '-';
-	display_gameboard(one, two, three, four, five, six, seven, eight, nine);
 	int turn = 0;
 	bool win = 0;
 	while (true) {
@@ -23,39 +21,48 @@ int main() {
 		if (turn % 2 == 0) {
 			cout << "X, it is your turn! Choose a place (1-9): ";
 			cin >> move_choice;
+			if (move_choice > 10 || move_choice <= 0) {
+				cout << "Please choose a section on the board!\n";
+				continue;
+			}
 			switch (move_choice) {
 			case 1:
-				one = 'X';
+				board[0] = 'X';
 				break;
 			case 2:
-				two = 'X';
+				board[1] = 'X';
 				break;
 			case 3:
-				three = 'X';
+				board[2] = 'X';
 				break;
 			case 4:
-				four = 'X';
+				board[3] = 'X';
 				break;
 			case 5:
-				five = 'X';
+				board[4] = 'X';
 				break;
 			case 6:
-				six = 'X';
+				board[5] = 'X';
 				break;
 			case 7:
-				seven = 'X';
+				board[6] = 'X';
 				break;
 			case 8:
-				eight = 'X';
+				board[7] = 'X';
 				break;
 			case 9:
-				nine = 'X';
+				board[8] = 'X';
+				break;
+			default:
+				cout << "Please choose a section of the board!";
 				break;
 			}
 			cout << "\n";
-			display_gameboard(one, two, three, four, five, six, seven, eight, nine);
+			display_gameboard(board);
 			turn += 1;
-			if ((one == 'X' && two == 'X' && three == 'X') || (four == 'X' && five == 'X' && six == 'X')) {
+			if ((one == 'X' && two == 'X' && three == 'X') || (four == 'X' && five == 'X' && six == 'X') || (seven == 'X' && eight == 'X' && nine == 'X') ||
+				(one == 'X' && four == 'X' && seven == 'X') || (two == 'X' && five == 'X' && eight == 'X') || (three == 'X' && six == 'X' && nine == 'X') ||
+				(one == 'X' && five == 'X' && nine == 'X') || (three == 'X' && five == 'X' && seven == 'X')) {
 				win = 1;
 			}
 			if (!win) {
@@ -70,37 +77,47 @@ int main() {
 			cin >> move_choice;
 			switch (move_choice) {
 			case 1:
-				one = 'O';
+				board[0] = 'O';
 				break;
 			case 2:
-				two = 'O';
+				board[1] = 'O';
 				break;
 			case 3:
-				three = 'O';
+				board[2] = 'O';
 				break;
 			case 4:
-				four = 'O';
+				board[3] = 'O';
 				break;
 			case 5:
-				five = 'O';
+				board[4] = 'O';
 				break;
 			case 6:
-				six = 'O';
+				board[5] = 'O';
 				break;
 			case 7:
-				seven = 'O';
+				board[6] = 'O';
 				break;
 			case 8:
-				eight = 'O';
+				board[7] = 'O';
 				break;
 			case 9:
-				nine = 'O';
+				board[8] = 'O';
 				break;
 			}
 			cout << "\n";
-			display_gameboard(one, two, three, four, five, six, seven, eight, nine);
+			display_gameboard(board);
 			turn += 1;
-			continue;
+			if ((one == 'O' && two == 'O' && three == 'O') || (four == 'O' && five == 'O' && six == 'O') || (seven == 'O' && eight == 'O' && nine == 'O') ||
+				(one == 'O' && four == 'O' && seven == 'O') || (two == 'O' && five == 'O' && eight == 'O') || (three == 'O' && six == 'O' && nine == 'O') ||
+				(one == 'O' && five == 'O' && nine == 'O') || (three == 'O' && five == 'O' && seven == 'O')) {
+				win = 1;
+			}
+			if (!win) {
+				continue;
+			}
+			else {
+				break;
+			}
 		}
 		else {
 			cout << "How even?!";
@@ -120,8 +137,9 @@ void intro_text() {
 		<< "Let's Begin!\n\n";
 }
 
-void display_gameboard(char oneP, char twoP, char threeP, char fourP, char fiveP, char sixP, char sevenP, char eightP, char nineP) {
-	cout << sevenP << "  |  " << eightP << "  |  " << nineP << endl
-		<< fourP << "  |  " << fiveP << "  |  " << sixP << endl
-		<< oneP << "  |  " << twoP << "  |  " << threeP << endl << endl;
+void display_gameboard(vector<char> boardish) {
+	vector<char> board = boardish;
+	cout << board[6] << "  |  " << board[7] << "  |  " << board[8] << '\n'
+		<< board[3]	<< "  |  " << board[4] << "  |  " << board[5] << '\n'
+		<< board[0] << "  |  " << board[1] << "  |  " << board[2] << "\n\n";
 }
