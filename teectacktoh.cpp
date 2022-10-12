d95627e926165604008b9f99b7cfb82ff62a7078
WitchHat <jack111one1@gmail.com>
2022-10-12 04:59:45 -0400

    Fix fucked up commit

--- teectacktoh.cpp
+++ teectacktoh.cpp
@@ -4,22 +4,6 @@
 
 using namespace std;
 
-<<<<<<< Updated upstream
-void display_gameboard(vector<char> boardish);
-vector<char> board;
-int turn = 0;
-char player;
-bool win = 0;
-char champ;
-char continue_choice = 'y';
-int move_choice;
-
-int Main() {
-	IntroText();
-
-	for (int i = 1; i <= 9; i++) {
-		board.push_back('-');
-=======
 // Params
 vector<char> board;
 int boardSize = 9;
@@ -60,7 +44,6 @@ bool ValidChoice(int input) {
 	if(input < 1 || input > 9) {
 		cout << "> That's not a valid input dude. Gotta be a number between 1 and 9 atleast...\n";	
 		return false;
->>>>>>> Stashed changes
 	}
 	else if(board[input - 1] != '-') {
 		cout << "> That spot isn't free, champ. Try again!\n";	
@@ -71,43 +54,6 @@ bool ValidChoice(int input) {
 int GetPlayerChoice() {
 	cout << "> " << player << ", it is your turn! Choose a place (1-9):\n";
 
-<<<<<<< Updated upstream
-	while (tolower(continue_choice) == 'y') {
-		display_gameboard(board);
-		while (!win) {
-			if (turnIsOdd()) player = 'X';
-			else player = 'O';
-
-			for (;;) {
-				cout << player << ", it is your turn! Choose a place (1-9): ";
-				if (cin >> move_choice) {
-					break;
-				}
-				else {
-					cout << "\nPlease enter a position 1-9!\n\n";
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				}
-			}
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				
-			if (move_choice > 10 || move_choice <= 0 || board[move_choice - 1] != '-') {
-				cout << "\nPlease choose an available section on the board!\n\n";
-				continue;
-			}
-
-			board[move_choice - 1] = player;
-			cout << "\n";
-			display_gameboard(board);
-			
-			turn++;
-		}
-
-		board.clear();
-		for (int i = 1; i <= 9; i++) {
-			board.push_back('-');
-=======
 	cin >> playerChoice;
 	while(true)
 	{
@@ -117,7 +63,6 @@ int GetPlayerChoice() {
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			//cout << "Uhh bad input\n";
 			cin >> playerChoice;
->>>>>>> Stashed changes
 		}
 		else break;
 	}
@@ -132,48 +77,6 @@ bool PlayerHasWon() {
 		(board[0] == player && board[3] == player && board[6] == player) || (board[1] == player && board[4] == player && board[7] == player) || (board[2] == player && board[5] == player && board[8] == player) ||
 		(board[0] == player && board[4] == player && board[8] == player) || (board[2] == player && board[4] == player && board[6] == player)) {
 
-<<<<<<< Updated upstream
-void IntroText() {
-	cout << "Tic-Tac-Toe!\n\n"
-		<< "Rules: X starts first! Type 1-9 to input your move! The game will automatically alternate turns.\n"
-		<< "This is the gameboard. It is sorted like a numpad: \n\n"
-		<< "7  |  8  |  9\n"
-		<< "4  |  5  |  6\n"
-		<< "1  |  2  |  3\n"
-		<< "\nThree X's or O's wins!\n"
-		<< "Let's Begin!\n\n";
-}
-
-bool PlayerHasWon() {
-	if ((board[0] == player && board[1] == player && board[2] == player) || (board[3] == player && board[4] == player && board[5] == player) || (board[6] == player && board[7] == player && board[8] == player) ||
-		(board[0] == player && board[3] == player && board[6] == player) || (board[1] == player && board[4] == player && board[7] == player) || (board[2] == player && board[5] == player && board[8] == player) ||
-		(board[0] == player && board[4] == player && board[8] == player) || (board[2] == player && board[4] == player && board[6] == player)) 
-	{
-		win = 1;
-		champ = player;
-		return true;
-	}
-	else return false;
-}
-
-bool TurnIsOdd()
-{
-	turn % 2 == 0
-}
-
-bool diagonal_win
-
-void ClearBoard()
-{
-	
-}
-
-void display_gameboard(vector<char> charVec) {
-	vector<char> board = charVec;
-	cout << board[6] << "  |  " << board[7] << "  |  " << board[8] << '\n'
-		<< board[3] << "  |  " << board[4] << "  |  " << board[5] << '\n'
-		<< board[0] << "  |  " << board[1] << "  |  " << board[2] << "\n\n";
-=======
 		champ = player;
 		if(player == 'X') xWins++;
 		else oWins++;
@@ -207,7 +110,6 @@ int main() {
 		EndOrContinue();
 	}
 
-	cout << "> Alright, ‚³‚æ‚¤‚È‚ç.";
+	cout << "> Alright, ã•ã‚ˆã†ãªã‚‰.";
 	return 0;
->>>>>>> Stashed changes
 }
